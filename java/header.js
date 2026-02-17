b003a51fe1c9c50e2a3b9774cecf6eda0475e51c
RedaEL <RedaElmourabit>
2026-02-17 10:12:49 +0100

    Final Release V1.05

---
+++ java/header.js
@@ -0,0 +1,7 @@
+
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Erreur lors du chargement du header :', error));
